ff5fed3eb9f59012ae13a61c3dfe98f1e5520110
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-08-31 18:27:58 -0300

    Add files via upload

---
+++ EX1POOMC/Retangulo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX1POOMC
+{
+    internal class Retangulo
+    {
+        private double baseRetangulo;
+
+        private double alturaRetangulo;
+
+
+
+        public Retangulo(double baseRetangulo, double alturaRetangulo)
+
+        {
+
+            this.baseRetangulo = baseRetangulo;
+
+            this.alturaRetangulo = alturaRetangulo;
+
+        }
+
+
+
+        public double CalcularArea()
+
+        {
+
+            return baseRetangulo *alturaRetangulo;
+
+        }
+    }
+}
